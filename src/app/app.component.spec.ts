0be4ebeef4ac17639b225a5e184567901f46cce3
Oscar <munoz.bernales.oscar@gmail.com>
2022-12-17 18:17:44 -0300

    UPD: update angular version

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'omunozb'`, () => {
+  it(`should have as title 'omunozb-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('omunozb');
+    expect(app.title).toEqual('omunozb-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('omunozb app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('omunozb-web app is running!');
   });
 });
